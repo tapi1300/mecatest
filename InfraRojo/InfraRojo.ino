d1ed8a0f88bdf98091f36e602fea64d00bd2383e
tapi1300 <pedop4@gmail.com>
2021-12-14 14:55:17 +0100

    Programa de prueba sensor infrarojos

---
+++ InfraRojo/InfraRojo.ino
@@ -0,0 +1,18 @@
+const int sensorPin = 11;
+void setup() {
+  Serial.begin(9600);   //iniciar puerto serie
+  pinMode(sensorPin , INPUT);  //definir pin como entrada
+}
+ 
+void loop(){
+  int value = 0;
+  value = digitalRead(sensorPin );  //lectura digital de pin
+ 
+  Serial.print(value);
+  Serial.print("\t");
+  if(value == 0)
+  {
+  Serial.println("Detectado obstaculo");
+  }
+  delay(1000);
+}
