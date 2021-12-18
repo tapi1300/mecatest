f3b845081e8082a28f2cdfb4abf4b624d75c260b
tapi1300 <pedop4@gmail.com>
2021-12-18 13:14:00 +0100

    Limpieza del código

--- pruebas_finales/mov_lineal/mov_lineal.ino
+++ pruebas_finales/mov_lineal/mov_lineal.ino
@@ -1,33 +1,32 @@
 #include <Servo.h>
 
-const int US_Trigger = 2;   //Pin digital 2 para el Trigger del sensor
-const int US_Echo = 3;   //Pin digital 3 para el Echo del sensor
+const int US_Trigger = 2;
+const int US_Echo = 3;
 const int pin_rueda_DER = 5;
 const int pin_rueda_IZQ = 6;
 const int IR_IZQ = 11;
 const int IR_DER = 10;
 
-long time_eco; //timepo que demora en llegar el eco
-long dist; //distancia en centimetros
+long time_eco;
+long dist;
 
 Servo myservoD;
 Servo myservoI;
 
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);   //iniciar puerto serie
+  Serial.begin(9600);
   myservoD.attach(pin_rueda_DER);
   myservoI.attach(pin_rueda_IZQ);
 
   pinMode(pin_rueda_DER,OUTPUT);
   pinMode(pin_rueda_IZQ,OUTPUT);
-  pinMode(IR_IZQ , INPUT);  //definir pin como entrada
-  pinMode(IR_DER , INPUT);  //definir pin como entrada
+  pinMode(IR_IZQ , INPUT);
+  pinMode(IR_DER , INPUT);
 
-  pinMode(US_Trigger, OUTPUT); //pin como salida
-  pinMode(US_Echo, INPUT);  //pin como entrada
-  digitalWrite(US_Trigger, LOW);//Inicializamos el pin con 0
+  pinMode(US_Trigger, OUTPUT);
+  pinMode(US_Echo, INPUT);
+  digitalWrite(US_Trigger, LOW);
 
 }
 
@@ -37,16 +36,16 @@ void loop() {
 
   int value_izq = 0;
   int value_der = 0;
-  value_izq = digitalRead(IR_IZQ);  //lectura digital de pin
-  value_der = digitalRead(IR_DER);  //lectura digital de pin
+  value_izq = digitalRead(IR_IZQ);
+  value_der = digitalRead(IR_DER);
 
   // ULTRASONIDO
   digitalWrite(US_Trigger, HIGH);
-  delayMicroseconds(10);          //Enviamos un pulso de 10us
+  delayMicroseconds(10);
   digitalWrite(US_Trigger, LOW);
   
-  time_eco = pulseIn(US_Echo, HIGH); //obtenemos el ancho del pulso
-  dist = time_eco/59;             //
+  time_eco = pulseIn(US_Echo, HIGH);
+  dist = time_eco/59;
 
   int free_path = 1;
   if(dist < 11)
@@ -57,8 +56,8 @@ void loop() {
   // SI HAY COLOR NEGRO EN LOS INFRARROJOS PARAMOS
   if(value_izq == 0 || value_der == 0)
   {
-    myservoD.write(90);  // set servo to mid-point
-    myservoI.write(90);  // set servo to mid-point
+    myservoD.write(90);
+    myservoI.write(90);
   }
   // SI NO, NOS MOVEMOS
   else if(value_izq == 1 && value_der == 1)
@@ -67,16 +66,16 @@ void loop() {
     if(free_path == 0) // HAY OBSTACULO
     {
       Serial.print("Distancia: ");
-      Serial.print(dist);      //Enviamos serialmente el valor de la distancia
+      Serial.print(dist);
       Serial.print("cm");
       Serial.println();
-      myservoD.write(180);  // set servo to mid-point
-      myservoI.write(0);  // set servo to mid-point
+      myservoD.write(180);
+      myservoI.write(0);
     }
     // SI NO, NOS MOVEMOS TRANQUILOS
     else{
-      myservoD.write(110);  // set servo to mid-point
-      myservoI.write(70);  // set servo to mid-point
+      myservoD.write(110);
+      myservoI.write(70);
     }
   }
 }
